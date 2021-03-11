621faf2fb8eb8ac8884587ebce5a3b2454edf250
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-03-11 20:58:16 +0800

    21-3-11

---
+++ 21-3-11/main.cpp
@@ -0,0 +1,48 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Hello world!" << endl;
+//    return 0;
+//}
+//#include<iostream>
+//#include<vector>
+//#include<algorithm>
+//using namespace std;
+//int main()
+//{
+//    vector<int>v;
+//    v.push_back(10);
+//    v.push_back(20);
+//    v.push_back(30);
+//    v.push_back(40);
+//    vector<int>::iterator itBegin=v.begin();
+//    vector<int>::iterator itend=v.end();
+//    while(itBegin!=itend)
+//    {
+//        cout<<*itBegin<<endl;
+//        itBegin++;
+//    }
+//    return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//#include<vector>
+//#include<algorithm>
+//template <class T>
+//void Print(T&p)
+//{
+//    cout<<p<<endl;
+//}
+//int main()
+//{
+//    vector<int>v;
+//    v.push_back(10);
+//    v.push_back(20);
+//    for(vector<int>::iterator it=v.begin();it!=v.end();it++)
+//        cout<<*it<<endl;
+//    for_each(v.begin(),v.end(),Print<int>);
+//    return 0;
+//}
